2a5165857fc89744af7b855c7d25b2e7275a05e3
VY <vlknyzc@gmail.com>
2023-10-22 01:57:51 +0100

    signal remove

--- master_data/signals.py
+++ master_data/signals.py
@@ -4,16 +4,16 @@ from django.dispatch import receiver
 from master_data.models import Workspace, Field
 
 
-@receiver(post_save, sender=Workspace)
-def create_fields_for_new_workspace(sender, instance, created, **kwargs):
-    if created:  # Check if this is a newly created object
-        with open('path_to_your_initial_fields.json', 'r') as file:
-            fields_data = json.load(file)
+# @receiver(post_save, sender=Workspace)
+# def create_fields_for_new_workspace(sender, instance, created, **kwargs):
+#     if created:  # Check if this is a newly created object
+#         with open('path_to_your_initial_fields.json', 'r') as file:
+#             fields_data = json.load(file)
 
-        for field_data in fields_data:
-            Field.objects.create(
-                platform=...,  # Assign your platform here
-                name=field_data['name'],
-                field_level=field_data['field_level']
-                # ... other fields ...
-            )
+#         for field_data in fields_data:
+#             Field.objects.create(
+#                 platform=...,  # Assign your platform here
+#                 name=field_data['name'],
+#                 field_level=field_data['field_level']
+#                 # ... other fields ...
+#             )
