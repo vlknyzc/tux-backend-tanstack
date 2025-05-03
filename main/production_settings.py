18b51fee17c79a50d8148e74ea8c0bbe19c18e07
VY <vlknyzc@gmail.com>
2025-05-03 20:40:04 +0100

    pg driver

--- main/production_settings.py
+++ main/production_settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "main.wsgi.application"
 # ────────────────────────────────────────────────────────────────
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME":     os.environ["PGDATABASE"],
         "USER":     os.environ["PGUSER"],
         "PASSWORD": os.environ["PGPASSWORD"],
