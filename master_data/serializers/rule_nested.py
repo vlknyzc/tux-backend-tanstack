e39c46b1526e73aa55cc421ad6c92ac58f9a7374
VY <vlknyzc@gmail.com>
2025-05-11 21:02:09 +0100

    rule nested read only update

--- master_data/serializers/rule_nested.py
+++ master_data/serializers/rule_nested.py
@@ -9,26 +9,29 @@ class RuleDetailNestedSerializer(serializers.ModelSerializer):
     dimension = serializers.PrimaryKeyRelatedField(
         queryset=models.Dimension.objects.all())
     dimension_order = serializers.IntegerField()
-    field_level = serializers.IntegerField(source='field.field_level')
-    field_name = serializers.CharField(source='field.name')
+    field_level = serializers.IntegerField(
+        source='field.field_level', read_only=True)
+    field_name = serializers.CharField(source='field.name', read_only=True)
 
     class Meta:
         model = models.RuleDetail
         fields = ['field', 'dimension', 'dimension_order',
                   'prefix', 'suffix', 'delimiter', 'field_level', 'field_name']
+        read_only_fields = ['field_level', 'field_name']
 
 
 class RuleNestedSerializer(serializers.ModelSerializer):
     rule_details = RuleDetailNestedSerializer(many=True)
     platform = serializers.PrimaryKeyRelatedField(
         queryset=models.Platform.objects.all())
-
-    platform_name = serializers.CharField(source='platform.name')
+    platform_name = serializers.CharField(
+        source='platform.name', read_only=True)
 
     class Meta:
         model = models.Rule
         fields = ['id', 'name', 'description',
                   'platform', 'status', 'rule_details', 'platform_name']
+        read_only_fields = ['platform_name']
 
     @transaction.atomic
     def create(self, validated_data):
