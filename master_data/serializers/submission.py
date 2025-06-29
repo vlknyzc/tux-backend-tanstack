2e39bb5811bc6ba2f1a391f0e3b313acfcfd7aa8
VY <vlknyzc@gmail.com>
2025-06-29 20:12:55 +0100

    submission serializer

--- master_data/serializers/submission.py
+++ master_data/serializers/submission.py
@@ -6,14 +6,19 @@ from typing import Optional, Dict, List, Any
 class SubmissionSerializer(serializers.ModelSerializer):
     created_by_name = serializers.SerializerMethodField()
     workspace_name = serializers.SerializerMethodField()
+    rule_name = serializers.SerializerMethodField()
+    platform = serializers.SerializerMethodField()
+    platform_name = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Submission
         fields = ['id', 'name', 'slug', 'description', 'status',
-                  'rule', 'selected_parent_string', 'starting_field',
+                  'rule', 'rule_name', 'platform', 'platform_name',
+                  'selected_parent_string', 'starting_field',
                   'workspace', 'workspace_name', 'created_by', 'created_by_name',
                   'created', 'last_updated']
-        read_only_fields = ['workspace_name']
+        read_only_fields = ['workspace_name',
+                            'rule_name', 'platform', 'platform_name']
 
     def get_created_by_name(self, obj) -> Optional[str]:
         if obj.created_by:
@@ -23,4 +28,19 @@ class SubmissionSerializer(serializers.ModelSerializer):
     def get_workspace_name(self, obj) -> Optional[str]:
         if obj.workspace:
             return obj.workspace.name
+        return None
+
+    def get_rule_name(self, obj) -> Optional[str]:
+        if obj.rule:
+            return obj.rule.name
+        return None
+
+    def get_platform(self, obj) -> Optional[int]:
+        if obj.rule and obj.rule.platform:
+            return obj.rule.platform.id
+        return None
+
+    def get_platform_name(self, obj) -> Optional[str]:
+        if obj.rule and obj.rule.platform:
+            return obj.rule.platform.name
         return None
